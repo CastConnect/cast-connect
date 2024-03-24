557a6105ddfac8f018d8fa3800139ccc7698fe8b
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-03-24 14:13:32 +0530

    Add hub and storage route

---
+++ src/app/storage/route.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const HUB_HTTPS_URL =
+  process.env.HUB_HTTPS_URL || "http://nemes.farcaster.xyz:2281";
+
+const headers = {
+  "Content-Type": "application/json",
+  api_key: process.env.HUB_API_KEY,
+};
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const fid = data.fid;
+
+  const result = await axios.get(
+    `${HUB_HTTPS_URL}/v1/storageLimitsByFid?fid=${fid}`,
+    { headers }
+  );
+
+  console.log(result);
+
+  return new Response(JSON.stringify(result));
+}
